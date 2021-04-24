eb40d2a717e3d4771591bd53222f456cb837ce98
sebasFili <sebastianfilippelli@gmail.com>
2021-04-23 23:58:10 -0300

    ejemplode validate

--- Login/Register.aspx
+++ Login/Register.aspx
@@ -1,7 +1,10 @@
 ﻿<%@ Page Title="Registro de Nuevo Usuario" MasterPageFile="~/Site.Master" Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="PuroEscabio.Login.Register" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
+    <h2><%: Title %>.
+    </h2>
     <h3>La página de seguridad funciona.</h3>
     <p>Use this area to provide additional information.</p>
+    <asp:TextBox ID="TextBox1" runat="server" ControlToValidate="TextBox1"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="el campo debe ser completado" ControlToValidate="TextBox1"></asp:RequiredFieldValidator>
 </asp:Content>
--- Login/Register.aspx.designer.cs
+++ Login/Register.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace PuroEscabio.Login
 
     public partial class Register
     {
+
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
     }
 }
