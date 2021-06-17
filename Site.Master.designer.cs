a559922bebeb33a031032d4a9d33ba35a065542e
Sebastian Filippelli <sebastianfilippelli@gmail.com>
2021-06-17 19:26:50 -0300

    ultimos cambios

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -33,15 +33,6 @@ namespace PuroEscabio
         protected global::System.Web.UI.WebControls.Label lblUsuarioLogueado;
 
         /// <summary>
-        /// linkCerrarSesion control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton linkCerrarSesion;
-
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
