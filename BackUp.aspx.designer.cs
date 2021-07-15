754bb18a029f3baa5ee0ee89723aa2ffbd4e32ec
nanchy14 <82817107+nanchy14@users.noreply.github.com>
2021-07-14 23:13:25 -0300

    BackUp
    Intente seguir pero me salta error. Hice a mi manera asi que cambia el conecction string

---
+++ BackUp.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PuroEscabio.Security
+{
+
+
+    public partial class Security
+    {
+
+        /// <summary>
+        /// Control btnBackUp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBackUp;
+
+        /// <summary>
+        /// Control listBackUp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox listBackUp;
+
+        /// <summary>
+        /// Control btnRestore.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRestore;
+    }
+}
